613b555c2e67382753afb258fef3e104965a80ff
Arjen Hiemstra <ahiemstra@heimr.nl>
2025-02-21 14:17:54 +0100

    Update database to work with SQLModel

--- database.py
+++ database.py
@@ -1,33 +1,24 @@
 import pathlib
 
-# import fastapi
-# import pydantic
+from sqlmodel import SQLModel
+from sqlmodel.ext.asyncio.session import AsyncSession
+
 import sqlalchemy
-import sqlalchemy.ext.declarative
-from sqlalchemy import Column, Boolean, Integer, String
 
 
 DatabaseUrl = f"sqlite+aiosqlite:///{pathlib.Path(__file__).resolve().parent}/krystalium.db"
 
 
+engine = sqlalchemy.ext.asyncio.create_async_engine(url = sqlalchemy.engine.make_url(DatabaseUrl))
 
 
-
-def async_session() -> sqlalchemy.orm.sessionmaker:
-    engine = sqlalchemy.ext.asyncio.create_async_engine(url = sqlalchemy.engine.make_url(DatabaseUrl))
-    return sqlalchemy.orm.sessionmaker(bind = engine, class_ = sqlalchemy.ext.asyncio.AsyncSession, expire_on_commit = False)
-
-
-class Connector:
-    @classmethod
-    async def get_session(cls):
-        session = async_session()
-        async with session() as db_session:
-            yield db_session
-            await db_session.rollback()
+async def get_session() -> AsyncSession:
+    session = sqlalchemy.orm.sessionmaker(bind = engine, class_ = AsyncSession, expire_on_commit = False)
+    async with session() as db_session:
+        yield db_session
+        await db_session.rollback()
 
 
 async def sqlalchemy_init():
-    engine = sqlalchemy.ext.asyncio.create_async_engine(url = sqlalchemy.engine.make_url(DatabaseUrl))
     async with engine.begin() as connection:
-        await connection.run_sync(Base.metadata.create_all)
+        await connection.run_sync(SQLModel.metadata.create_all)
